02e83784f4d31004a9254f4337fa9cd67704f749
syed waseem <sy.waseemofficial@gmail.com>
2025-07-23 17:50:13 +0530

    Update test.cy.js
    aaa212w321

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -2,6 +2,6 @@ describe("test_1",()=>{
 it("open google.com",()=>{
 cy.visit("https://example.cypress.io/");
 //cy.get(".g").type("cypress.io tutorial")
-cy.url().to.equal("https://example.cypress.io/");
+cy.url().should("eq", "https://example.cypress.io/");
 })
 })
